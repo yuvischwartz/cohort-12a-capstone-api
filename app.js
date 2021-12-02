e2324460fd42f127091d6e5c48800a61655da77b
yuvischwartz <yuval.orderdesk@gmail.com>
2021-12-02 10:49:04 -0200

    I added 'cors' package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 require('./lib/models')
 
 
@@ -11,6 +12,7 @@ var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
